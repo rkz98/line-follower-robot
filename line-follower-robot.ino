c1ca5724a10bb6bb9d79827b9fb5fb9605480196
Kauê Rodrigues <kauerodrigues98@gmail.com>
2020-09-29 23:38:58 -0300

    caralho!

--- line-follower-robot.ino
+++ line-follower-robot.ino
@@ -1,12 +1,18 @@
 #define PIN_ENABLE 7
 
-#define BIT_MOTOR_1_A 5
-#define BIT_MOTOR_1_B 7
-#define BIT_MOTOR_2_A 0
-#define BIT_MOTOR_2_B 6
+#define BIT_MOTOR_1_A 2
+#define BIT_MOTOR_1_B 3
+#define BIT_MOTOR_2_A 1
+#define BIT_MOTOR_2_B 4
+#define BIT_MOTOR_3_A 5
+#define BIT_MOTOR_3_B 7
+#define BIT_MOTOR_4_A 0
+#define BIT_MOTOR_4_B 6
 
-#define PIN_MOTOR_2_PWM 6
-#define PIN_MOTOR_1_PWM 5
+#define PIN_MOTOR_3_PWM 5
+#define PIN_MOTOR_2_PWM 3
+#define PIN_MOTOR_4_PWM 6
+#define PIN_MOTOR_1_PWM 11
 
 #define COMPLETE_ROTATION 20
 
@@ -19,10 +25,11 @@
 #define PIN_LASER_SENSOR_LEFT_BACK A3
 #define PIN_LASER_SENSOR_RIGHT_BACK A4
 
-#define MOTOR_VELOCITY_0 25
-#define MOTOR_VELOCITY_1 80
-#define MOTOR_VELOCITY_2 180
-#define MOTOR_VELOCITY_3 255
+#define MOTOR_VELOCITY_0 0
+#define MOTOR_VELOCITY_1 25
+#define MOTOR_VELOCITY_2 80
+#define MOTOR_VELOCITY_3 180
+#define MOTOR_VELOCITY_4 255
 
 unsigned int rotationStep = 0;
 unsigned int completeRotations = 0;
@@ -35,6 +42,8 @@ void setup() {
 
 	pinMode(PIN_MOTOR_1_PWM, OUTPUT);
 	pinMode(PIN_MOTOR_2_PWM, OUTPUT);
+	pinMode(PIN_MOTOR_3_PWM, OUTPUT);
+	pinMode(PIN_MOTOR_4_PWM, OUTPUT);
 
 	pinMode(PIN_LASER_SENSOR_LEFT, INPUT);
 	pinMode(PIN_LASER_SENSOR_RIGHT, INPUT);
@@ -51,8 +60,16 @@ void setup() {
 	digitalWrite(BIT_MOTOR_2_B, LOW);
 	digitalWrite(BIT_MOTOR_2_A, HIGH);
 
+	digitalWrite(BIT_MOTOR_3_B, LOW);
+	digitalWrite(BIT_MOTOR_3_A, HIGH);
+
+	digitalWrite(BIT_MOTOR_4_B, LOW);
+	digitalWrite(BIT_MOTOR_4_A, HIGH);
+
 	setMotorSpeed(PIN_MOTOR_1_PWM, MOTOR_VELOCITY_3);
 	setMotorSpeed(PIN_MOTOR_2_PWM, MOTOR_VELOCITY_3);
+	setMotorSpeed(PIN_MOTOR_2_PWM, MOTOR_VELOCITY_3);
+	setMotorSpeed(PIN_MOTOR_4_PWM, MOTOR_VELOCITY_3);
 }
 
 void setMotorSpeed(unsigned int pinPwmToUpdate, unsigned int speedValue) {
@@ -103,6 +120,17 @@ void startRunningMotor() {
 		setMotorSpeed(PIN_MOTOR_2_PWM, MOTOR_VELOCITY_3);
 	}
 
+//    if (laserSensorLeftState <= MIN_ANALOG_VALUE) {
+//      setMotorSpeed(PIN_MOTOR_1_PWM, MOTOR_VELOCITY_2);
+//      setMotorSpeed(PIN_MOTOR_2_PWM, MOTOR_VELOCITY_3);
+//      setMotorSpeed(PIN_MOTOR_3_PWM, MOTOR_VELOCITY_0);
+//      setMotorSpeed(PIN_MOTOR_4_PWM, MOTOR_VELOCITY_0);
+//    } else {
+//      setMotorSpeed(PIN_MOTOR_1_PWM, MOTOR_VELOCITY_3);
+//      setMotorSpeed(PIN_MOTOR_2_PWM, MOTOR_VELOCITY_2);
+//      setMotorSpeed(PIN_MOTOR_3_PWM, MOTOR_VELOCITY_3);
+//      setMotorSpeed(PIN_MOTOR_4_PWM, MOTOR_VELOCITY_3);
+//    }
 }
 
 void loop() {
